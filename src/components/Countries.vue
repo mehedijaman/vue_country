7e76a0e4f7d44caaf78a4b97bdc4537caae40d37
Mehedi Jaman <mail4mjaman@gmail.com>
2023-07-26 20:33:04 +0600

    Live search added

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -5,6 +5,9 @@ import {ref, reactive} from 'vue'
 const countries = reactive([]);
 const filterdCountries = reactive([])
 
+let totalCountries = ref(0)
+let totalFilteredCountries = ref(0)
+
 function getAllCountries(){
   fetch('https://restcountries.com/v3.1/all')
   .then(response => response.json())
@@ -15,18 +18,27 @@ function getAllCountries(){
       countries.push(country)
       filterdCountries.push(country)
     })
+
+    totalCountries = countries.length
+    totalFilteredCountries = filterdCountries.length
   })
   .catch(error => {
     console.log('Error getting country data: ', error)
   })
 }
 
+function clearCountries(){
+  countries.length = 0
+  filterdCountries.length = 0
+}
 
 function filterCountry(firstLetter){
   filterdCountries.length = 0
   countries.filter(c => c.name.common.startsWith(firstLetter)).forEach(element => {
-    filterdCountries.push(element)
+    filterdCountries.push(element)    
   })
+
+  totalFilteredCountries = filterdCountries.length
 }
 
 let text = '';
@@ -36,6 +48,8 @@ function searchCountry(text){
   countries.filter(c => c.name.common.toLowerCase().includes(text.toLowerCase())).forEach(element => {
     filterdCountries.push(element)
   })
+
+  totalFilteredCountries = filterdCountries.length
 }
 
 const filterButtons = reactive([])
@@ -50,8 +64,9 @@ for(let i = 65; i <= 90; i++){
     <div class="card">
       <div class="card-header">
         <div class="row mb-1">
-          <div class="col-md-2 offset-md-5">
-            <button @click="getAllCountries()" class="btn btn-primary btn-sm">Get All Countries</button>
+          <div class="col-md-4 offset-md-4">
+            <button @click="getAllCountries()" class="btn btn-primary btn-sm mx-1">Get All Countries</button>
+            <button @click="clearCountries()" class="btn btn-secondary btn-sm">Clear Countries</button>
           </div>
         </div>
         <div class="row">
@@ -62,13 +77,17 @@ for(let i = 65; i <= 90; i++){
             <input v-model="text" @keyup="searchCountry(text)" type="text" class="form-control" placeholder="Search...">
           </div>
         </div>
+
+        <div class="row">
+          <p>Shown {{ totalFilteredCountries }} of {{ totalCountries }} Countries</p>
+        </div>
         
       </div>
       <div class="card-body">
         <div class="row row-cols-1 row-cols-md-5 g-4">
           <div v-for="(country, index) in filterdCountries" :key="index" class="col">
             <div class="card h-100">
-              <img :src="country.flags.png" class="card-img-top img-responsive" :alt="country.flags.alt">
+              <img :src="country.flags.png" class="card-img-top" style="height:120px" :alt="country.flags.alt">
               <div class="card-body">
                 <h5 class="card-title">{{ country.name.common }}</h5>
                 <p class="card-text">{{ country.name.official }}</p>
